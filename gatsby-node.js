32ce7c6ced46432a8480b25711dcb867565a87cb
Nate Finch <finchproservices@gmail.com>
2020-02-06 21:39:55 -0600

    post graphql query works

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,41 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require(`path`);
 
-// You can delete this file if you're not using it
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  return graphql(`
+    query GET_POSTS {
+        wpgraphql {
+            posts {
+                edges {
+                node {
+                    title
+                    slug
+                    content(format: RENDERED)
+                    date
+                    featuredImage {
+                        altText
+                        link
+                        uri
+                    }
+                    author {
+                    name
+                    }
+                }
+                }
+            }
+        }
+    }
+  `).then(result => {
+    result.data.wpgraphql.posts.edges.forEach(({ node }) => {
+      createPage({
+        path: node.slug,
+        component: path.resolve(`./src/templates/blog-template.js`),
+        context: {
+          // This is the $slug variable
+          // passed to blog-post.js
+          slug: node.slug,
+        },
+      })
+    })
+  })
+}
