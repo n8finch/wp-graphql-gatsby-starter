8ea2a086dbcafdb0b4eda26c33b1230db2c9bd8e
Nate Finch <finchproservices@gmail.com>
2020-02-08 00:44:12 -0600

    update nav

--- src/components/menu.js
+++ src/components/menu.js
@@ -31,16 +31,16 @@ export default (props) => (
         {console.log(data.wpgraphql.menuItems.edges)}
          {data.wpgraphql.menuItems.edges.map(({ node }) => (
             <div key={node.id}>
-                <Link to={`${node.url}`}>
-                <div dangerouslySetInnerHTML={{ __html: node.label }} />
-                </Link>
+                {('#' !== node.url) ? (
+                    <Link to={`${node.url}`}>{node.label}</Link>
+                ) : (
+                    <div>{node.label}</div>
+                )}
                 {(node.childItems) && (
                     <ul>
                     {node.childItems.edges.map(({ node }) => (
                         <li key={node.id}>
-                            <Link to={`${node.url}`}>
-                            <div dangerouslySetInnerHTML={{ __html: node.label }} />
-                            </Link>
+                            <Link to={`${node.url}`}>{node.label}</Link>
                         </li>
                     ))}
                     </ul>
