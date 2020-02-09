a86eaea32e140a596114aa3012c5a124d2d9b6ad
Nate Finch <finchproservices@gmail.com>
2020-02-08 20:04:23 -0600

    update menu for actual related objects and update wp to flatten page structure

--- src/components/menu.js
+++ src/components/menu.js
@@ -11,41 +11,6 @@ export default () => (
                     node {
                     label
                     url
-                    childItems {
-                        edges {
-                        node {
-                            label
-                            url
-                            childItems {
-                            edges {
-                                node {
-                                connectedObject {
-                                    ... on WPGraphQL_Post {
-                                    slug
-                                    title
-                                    }
-                                    ... on WPGraphQL_MenuItem {
-                                    title
-                                    }
-                                    ... on WPGraphQL_Tag {
-                                    slug
-                                    name
-                                    }
-                                    ... on WPGraphQL_Category {
-                                    slug
-                                    name
-                                    }
-                                    ... on WPGraphQL_Page {
-                                    slug
-                                    title
-                                    }
-                                }
-                                }
-                            }
-                            }
-                        }
-                        }
-                    }
                     connectedObject {
                         ... on WPGraphQL_Post {
                         slug
@@ -67,6 +32,35 @@ export default () => (
                         title
                         }
                     }
+                    childItems {
+                        edges {
+                        node {
+                            label
+                            url
+                            connectedObject {
+                            ... on WPGraphQL_Post {
+                                slug
+                                title
+                            }
+                            ... on WPGraphQL_MenuItem {
+                                title
+                            }
+                            ... on WPGraphQL_Tag {
+                                slug
+                                name
+                            }
+                            ... on WPGraphQL_Category {
+                                slug
+                                name
+                            }
+                            ... on WPGraphQL_Page {
+                                slug
+                                title
+                            }
+                            }
+                        }
+                        }
+                    }
                     }
                 }
                 }
@@ -77,20 +71,18 @@ export default () => (
     render={data => (
       <nav>
         <ul>
-
-         {data.wpgraphql.menuItems.edges.map(({ node }) => (
+        {data.wpgraphql.menuItems.edges.map(({ node }) => (
             <li key={node.id}>
                 {('#' !== node.url) ? (
-                    <Link to={`${node.url}`}>{node.label}</Link>
+                    <Link to={`/${node.connectedObject.slug}`}>{node.label}</Link>
                 ) : (
                     <div>{node.label}</div>
                 )}
                 {(node.childItems.edges.length > 0) && (
                     <ul className="sub-nav">
-                        {console.log(node)}
                         {node.childItems.edges.map(({ node }) => (
                             <li key={node.id}>
-                                <Link to={`${node.url}`}>{node.label}</Link>
+                                <Link to={`/${node.connectedObject.slug}`}>{node.label}</Link>
                             </li>
                         ))}
                     </ul>
